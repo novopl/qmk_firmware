474c920adc2ddcc81face1f0be70a1e7036fa889
Mateusz Klos <novopl@gmail.com>
2024-08-23 03:22:58 +0100

    checkpoint

--- keyboards/sofle_choc/keymaps/novopl/keymap.c
+++ keyboards/sofle_choc/keymaps/novopl/keymap.c
@@ -80,28 +80,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //   KC_ESC,   M_LOPT(KC_A), M_LSFT(KC_S), M_LCTL(KC_D), M_LCMD(KC_F), KC_G,                     KC_H, M_RCMD(KC_J), M_RCTL(KC_K), M_RSFT(KC_L), M_ROPT(KC_SCLN),  KC_QUOT,
   //   KC_LSFT,  KC_Z,         KC_X,         KC_C,         KC_V,         KC_B, KC_MUTE,  KC_MPLY,  KC_N, KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          KC_RSFT,
   //                                   KC_LALT, KC_LCTL, KC_LGUI, MO(L_LOWER), KC_SPC,   KC_ENT,   MO(L_UPPER),  KC_RALT,  KC_RCMD, DF(L_COLEMAK)
-  // ),
+// ),
   [L_LOWER] = LAYOUT(
     QK_RBT,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                          KC_F6,    KC_F7,      KC_F8,    KC_F9,    KC_F10,     KC_F11,
     G(KC_TAB),  KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                           KC_6,     KC_7,       KC_8,     KC_9,     KC_0,       KC_BSPC,
-    KC_ENT,     S(KC_1),    S(KC_LBRC), S(KC_9),    S(KC_0),    S(KC_RBRC),                     S(KC_6),  S(KC_7),    S(KC_8),  KC_NO,    S(KC_SCLN), KC_BSLS,
-    KC_LSFT,    S(KC_DOT),  KC_MINS,    KC_LBRC,    KC_RBRC,    KC_NO ,   KC_MUTE,  KC_MPLY,    KC_NO,    KC_NO,      KC_COMM,    KC_DOT,   KC_NO,      KC_RSFT,
+    KC_ENT,     S(KC_1),    S(KC_LBRC), S(KC_9),    S(KC_0),    S(KC_RBRC),                     S(KC_6),  S(KC_7),    S(KC_8),  KC_NO,    KC_NO,      KC_BSLS,
+    KC_LSFT,    S(KC_DOT),  KC_MINS,    KC_LBRC,    KC_RBRC,    KC_NO ,   KC_MUTE,  KC_MPLY,    KC_NO,    KC_MINS,    KC_EQL,   KC_DOT,   KC_NO,      KC_RSFT,
                                     G(C(KC_Q)),  KC_UP,  KC_ENT, KC_NO,   KC_ENT,   G(KC_SPC),  TL_UPPR,  KC_RALT,    KC_LCTL,  KC_RCTL
   ),
   [L_UPPER] = LAYOUT(
-    KC_NUBS,    KC_F12,   KC_F13,     KC_F14,     KC_F15,   KC_F16,                         KC_F17,   KC_F18,     KC_F19,       KC_F20,     KC_F21,     KC_F22,
-    S(KC_NUBS), KC_NO,    KC_NO,      KC_NO,      KC_NO,    KC_NO,                          KC_DOWN,  KC_RIGHT,   S(KC_MINS),   KC_EQL,     KC_NO,      KC_BSPC,
-    KC_NUBS,    S(KC_1),  S(KC_2),    S(KC_3),    S(KC_4),  S(KC_5),                        KC_LEFT,  KC_NO,      KC_MINS,      S(KC_EQL),  S(KC_SCLN), S(KC_BSLS),
-    KC_LSFT,    KC_ACL2,  S(KC_MINS), KC_LBRC,    KC_RBRC,  S(KC_EQL),  KC_MUTE,  KC_MPLY,  KC_UP,    KC_NO,      KC_NO,        KC_NO,      KC_NO,      KC_RSFT,
+    KC_NUBS,    KC_F12,   KC_F13,     KC_F14,     KC_F15,   KC_F16,                     KC_F17,   KC_F18,     KC_F19,       KC_F20,     KC_F21,     KC_F22,
+    S(KC_NUBS), KC_NO,    KC_NO,      KC_NO,      KC_NO,    KC_NO,                      KC_DOWN,  KC_RIGHT,   S(KC_MINS),   KC_EQL,     KC_NO,      KC_BSPC,
+    KC_NUBS,    S(KC_1),  S(KC_2),    S(KC_3),    S(KC_4),  S(KC_5),                    KC_LEFT,  KC_NO,      KC_MINS,      S(KC_EQL),  KC_NO,      S(KC_BSLS),
+    KC_LSFT,    KC_NO,    KC_NO,      KC_LBRC,    KC_RBRC,  KC_NO,  KC_MUTE,  KC_MPLY,  KC_UP,    KC_MINS,    KC_EQL,        KC_NO,      KC_NO,      KC_RSFT,
                                   KC_LALT,  KC_LCTL,  KC_LCMD, TL_LOWR, A(KC_BSPC),  KC_SPC,  TT(L_UPPER),    KC_RALT,    KC_LCTL,      KC_RCTL
   ),
   [L_ADJUST] = LAYOUT(
-    KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,                      KC_NO,        C(A(KC_MINS)),  C(A(KC_EQL)),   KC_NO,          KC_NO,      KC_NO,
-    KC_NO,  KC_NO,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_WH_U,                    C(A(KC_D)),   C(A(KC_U)),     C(A(KC_UP)),    C(A(KC_I)),     C(A(KC_T)), KC_NO,
-    KC_NO,  KC_NO,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,                    KC_ACL1,      C(A(KC_LEFT)),  C(A(KC_BSPC)),  C(A(KC_RIGHT)), C(A(KC_D)), KC_NO,
-    KC_NO,  KC_NO,  KC_NO,    KC_WH_D,  KC_WH_U,  C(KC_LEFT), KC_NO,  KC_NO,  C(KC_RIGHT),  C(A(KC_J)),     C(A(KC_DOWN)),  C(A(KC_K)),     KC_NO,      KC_NO,
+    KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,                      KC_NO,    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,  KC_NO,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_WH_U,                    KC_DOWN,  KC_RIGHT,       C(A(KC_MINS)),  C(A(KC_EQL)),   C(A(KC_BSPC)),  KC_NO,
+    KC_NO,  KC_NO,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,                    KC_LEFT,  C(A(KC_LEFT)),  C(A(KC_U)),     C(A(KC_I)),     C(A(KC_D)),     KC_NO,
+    KC_NO,  KC_NO,  KC_NO,    KC_WH_D,  KC_WH_U,  C(KC_LEFT), KC_NO,  KC_NO,  KC_UP,    C(A(KC_D)),     C(A(KC_J)),     C(A(KC_K)),     C(A(KC_T)),     KC_NO,
                               KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO
   ),
+  // [L_ADJUST] = LAYOUT(
+  //   KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,                      KC_NO,        C(A(KC_MINS)),  C(A(KC_EQL)),   KC_NO,          KC_NO,      KC_NO,
+  //   KC_NO,  KC_NO,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_WH_U,                    KC_NO,        C(A(KC_U)),     C(A(KC_UP)),    C(A(KC_I)),     C(A(KC_T)), KC_NO,
+  //   KC_NO,  KC_NO,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,                    KC_ACL1,      C(A(KC_LEFT)),  C(A(KC_BSPC)),  C(A(KC_RIGHT)), C(A(KC_D)), KC_NO,
+  //   KC_NO,  KC_NO,  KC_NO,    KC_WH_D,  KC_WH_U,  C(KC_LEFT), KC_NO,  KC_NO,  C(KC_RIGHT),  C(A(KC_J)),     C(A(KC_DOWN)),  C(A(KC_K)),     KC_NO,      KC_NO,
+  //                             KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO
+  // ),
 };
 
 
